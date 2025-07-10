f89d145a48d73172fa9f0c9abbfaff9a169c3aff
cpark <cpark04217@gmail.com>
2025-07-11 00:14:04 +1000

    Updated Houses modelling

--- houses/admin.py
+++ houses/admin.py
@@ -3,4 +3,13 @@ from .models import House
 
 @admin.register(House) # decorator to register the model with the admin site
 class HouseAdmin(admin.ModelAdmin):
-    pass # inherits everything from the ModelAdmin class
+    
+    list_display = [
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed"
+    ]
+    list_filter = ["price_per_night", "pets_allowed"] # adds a filter to the admin site
+    search_fields = ["name", "address"] # adds a search bar to the admin site
+    ordering = ["price_per_night"] # adds a sorting option to the admin site
--- houses/models.py
+++ houses/models.py
@@ -5,11 +5,14 @@ class House(models.Model):
 
     """Model Definition for House"""
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=255)
+    pets_allowed = models.BooleanField(default=True)
     bedrooms = models.PositiveIntegerField()
     bathrooms = models.PositiveIntegerField()
 
-    
+# __str__ method is used to return a string representation of the object
+def __str__(self):
+    return self.name
     
